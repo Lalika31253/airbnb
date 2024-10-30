21127e13f171db39bab45e104c27c7e635f70ffd
Alina <Lalika31253@gmail.com>
2024-10-29 21:17:00 -0600

    Add some styles to the SearchBar component

--- app/search/components/SearchBar.jsx
+++ app/search/components/SearchBar.jsx
@@ -1,23 +1,35 @@
+'use client';
+
+import { useState } from "react";
 
 const SearchBar = () => {
 
+  const [isSearchFocused, setIsSearchFocused] = useState(false);
+
+
   return (
-    <div>
-      <button>
-        <p>Search destination</p>
+    <div className="flex flex-row self-center rounded-full border p-2 mt-8 w-3/4">
+      <button onClick={() => setIsSearchFocused(true)}>
+      <p className="font-bold">Where</p>
+        {
+          isSearchFocused ? (
+            <input type="text" placeholder="Search destination" className="text-slate-800 bg-transparent border-none outline-none"/>
+          ) : (
+            <p className= "text-slate-600">Search destination</p>
+          )}
       </button>
 
       <div>
         <label>
-          <p>Dates</p>
-          <p>Select Ranged</p>
+          <p className="font-bold">Dates</p>
+          <p className= "text-slate-600">Select Ranged</p>
         </label>
       </div>
 
       <div>
         <label>
-          <p>Who</p>
-          <p>Add Guests</p>
+          <p className="font-bold">Who</p>
+          <p className= "text-slate-600">Add Guests</p>
         </label>
       </div>
     </div>
